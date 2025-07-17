53e2b3ada563b8c03934e75198abe9154fdb7f46
Mauricio Duran <mauriciodurant@gmail.com>
2025-07-17 15:21:59 -0400

    add lang:es

---
+++ lang/es/auth.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'failed'   => 'Las credenciales no coinciden con nuestros registros.',
+    'password' => 'La contraseña es incorrecta.',
+    'throttle' => 'Demasiados intentos de acceso. Por favor intente nuevamente en :seconds segundos.',
+];
